1e2a16c739a356022ca4ea2f239df9c78673730c
Syed Usman Farooq <syedusmanfarooq@hotmail.com>
2022-06-29 11:21:57 +0200

    Prompts for the install script

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 #Creates a virtual environment in the directory 
+echo "[x] Creating a new virtual environment in the current directory"
 python3 -m venv venv
-source venv/bin/activate
 
+echo "[x] Acivating the new virtual environment named [venv]"
+./venv/bin/activate
+
+echo "[x] Installing packages for the project"
 # install the packages using pip
 pip install -r requirements.txt
 
+echo "[x] Initializing the Project and creating database "
 # Create database for the products 
 python -c "from main import db, create_app; db.create_all(app=create_app())"
 
+echo "[x] Starting Server"
 # Starts the server
 python3 main.py
